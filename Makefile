0f47c155764f95af5998207a492ef881194d47b4
alexis <alexis.auriac@epitech.eu>
2018-02-22 18:40:54 +0100

    error handling done

---
+++ Makefile
@@ -0,0 +1,20 @@
+##
+## EPITECH PROJECT, 2017
+## 107transfer
+## File description:
+## Makefile for 107transfer
+##
+
+NAME	=	107transfer
+
+all	:	$(NAME)
+
+$(NAME)	:
+		cp src/transfer.pl $(NAME)
+
+fclean	:
+		rm -f $(NAME)
+
+re	:	fclean	all
+
+.PHONY	:	all  fclean  re
---
+++ src/error.pm
@@ -0,0 +1,30 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+sub error {
+    if (scalar @ARGV == 0) {
+	return (84);
+    }
+    foreach my $pol (@ARGV) {
+	if (length $pol == 0) {
+	    return 84;
+	}
+	for (my $i = 0 ; $i < length $pol ; ++$i) {
+	    my $char = substr($pol, $i, 1);
+	    if (($char lt 1 || $char gt 9) && $char ne "*") {
+		return (84);
+	    }
+	    elsif ($char eq "*" && substr($pol, $i + 1, 1) eq "*") {
+		return (84);
+	    }
+	}
+	if (index($pol, "*") == 0 || index($pol, "*") == length($pol) - 1) {
+	    return (84);
+	}
+    }
+    return 0;
+}
+
+1;
