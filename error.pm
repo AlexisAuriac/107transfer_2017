5e79583fa8b328841b38ae65e6e1b30ed25141fc
alexis <alexis.auriac@epitech.eu>
2018-03-01 16:37:49 +0100

    An error in error handling made it so that some '*' in last position weren't detected (used index instead of rindex) added error messages on the error output to identify problems

--- error.pm
+++ error.pm
@@ -18,14 +18,26 @@ sub error_syntax {
 
     for (my $i = 0 ; $i < length $pol ; ++$i) {
 	my $char = substr($pol, $i, 1);
-	if (index("0123456789*", $char) == -1) {
+	my $next_char = substr($pol, $i + 1, 1);
+	if (index("0123456789*-", $char) == -1) {
+	    print STDERR "Synthax error: Invalid character\n";
 	    return (84);
 	}
-	elsif ($char eq "*" && substr($pol, $i + 1, 1) eq "*") {
+	elsif ($char eq "*" && $next_char eq "*") {
+	    print STDERR "Synthax error: two '*' in a row\n";
+	    return (84);
+	}
+	elsif ($next_char eq "-" && ($i != 0 || $char ne "*")) {
+	    print STDERR "Synthax error: bad character before '-'";
+	    return (84);
+	}
+	elsif ($char eq "-" && index("0123456789", $next_char) == -1) {
+	    print STDERR "Synthax error: bad character after '-'\n";
 	    return (84);
 	}
     }
-    if (index($pol, "*") == 0 || index($pol, "*") == length($pol) - 1) {
+    if (index($pol, "*") == 0 || rindex($pol, "*") == length($pol) - 1) {
+	print STDERR "Syntax error: '*' in first or last position\n";
 	return (84);
     }
 }
@@ -41,9 +53,11 @@ sub division_by_zero {
 	}
     }
     if ($turn % 2 == 0 && ($chars eq "0" || $chars eq "0*")) {
+	print STDERR "Division by zero 1\n";
 	return (84);
     }
     elsif ($turn % 2 == 0 && substr($pol, 0, 1) eq "0") {
+	print STDERR "Division by zero 2\n";
 	return (84);
     }
 }
@@ -55,11 +69,17 @@ sub error {
 	help();
 	exit(0);
     }
-    if (scalar @ARGV == 0 || scalar(@ARGV) % 2 != 0) {
+    if (scalar @ARGV == 0) {
+	print STDERR "Not enough arguments\n";
+	return (84);
+    }
+    if (scalar(@ARGV) % 2 != 0) {
+	print STDERR "Odd number of arguments\n";
 	return (84);
     }
     foreach my $pol (@ARGV) {
 	if (length $pol == 0) {
+	    print STDERR "Empty argument\n";
 	    return (84);
 	}
 	if (error_syntax($pol) == 84 || division_by_zero($pol, $turn)) {
