d3485e9f51403785cd9367dffff4ca6ab719bf6c
alexis <alexis.auriac@epitech.eu>
2019-09-12 13:54:07 +0200

    Added SublimeText files to .gitignore

--- transfer.pl
+++ transfer.pl
@@ -12,12 +12,11 @@ if (error() == 84) {
 for (my $x = 0 ; $x < 1.001 ; $x += 0.001) {
     my $result = 1;
     for (my $i = 0 ; $i < scalar(@ARGV) ; ++$i) {
-	if ($i % 2 == 0) {
-	    $result *= compute_pol($ARGV[$i], $x);
-	}
-	else {
-	    $result /= compute_pol($ARGV[$i], $x);
-	}
+        if ($i % 2 == 0) {
+            $result *= compute_pol($ARGV[$i], $x);
+        } else {
+            $result /= compute_pol($ARGV[$i], $x);
+        }
     }
     $x = eval sprintf("%.3f", $x);
     print "$x -> ";
